c51a3f31881d7fe1e9f36f8e37df407f929e5704
ClockworkBlue <gianluca@dalmas.ch>
2017-08-05 16:31:16 +0300

    allow cross origin for web ui

--- src/main/java/cwb/fizzbuzz/Controller.java
+++ src/main/java/cwb/fizzbuzz/Controller.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+	@CrossOrigin
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public List<String> root(@RequestBody List<BigInteger> list) {
 		return list.stream()
