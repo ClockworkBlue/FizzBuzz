cb4202170bfab28b67ad54c269e975348616e24f
ClockworkBlue <gianluca@dalmas.ch>
2017-08-06 12:47:48 +0300

    added catch-all redirection mapping to controller

--- src/main/java/cwb/fizzbuzz/Controller.java
+++ src/main/java/cwb/fizzbuzz/Controller.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class Controller {
@@ -18,5 +19,10 @@ public class Controller {
 		return list.stream()
 				.map(integer -> FizzBuzz.play(integer))
 				.collect(Collectors.toList());
+	}
+
+	@RequestMapping(value = "**", method = RequestMethod.GET)
+	public ModelAndView getAnythingelse() {
+		return new ModelAndView("redirect:http://fizzbuzz.dalmas.ch");
 	}
 }
